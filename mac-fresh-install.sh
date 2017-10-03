bc82f4b694acc0f6a1296eb8f689a8829e59af2f
Pablo Varela <pablovarela182@gmail.com>
2017-10-03 20:45:56 +0200

    Really cool format

--- mac-fresh-install.sh
+++ mac-fresh-install.sh
@@ -5,58 +5,73 @@ ld="\x01$(tput bold)\x02"
 normal="\x01$(tput sgr0)\x02"
 cyan="\x01\033[36m\x02"
 green="\x01\033[32m\x02"
-step_symbol="↪"
+dot="⦿"
+arrow="↪"
 
 function pr {
-  echo -e "$cyan$bold$step_symbol $1$normal"
+  echo -e "$cyan$bold$arrow $1$normal"
+}
+
+function step {
+  echo -e "$green$bold$dot $1$normal"
 }
 
 function is {
   hash $1 2>/dev/null && true || false
 }
 
+function brewy {
+  pr "Installing $1"
+  is $1 || brew install $1
+}
+
+function casky {
+  pr "Installing $1"
+  brew cask install $1 2> /dev/null
+}
+
 # Install command line tools
-pr "Installing command line tools"
+step "XCODE COMMAND LINE TOOLS"
 is gcc || xcode-select --install
 
 # Install homebrew
-pr "Installing homebrew.."
+step "HOMEBREW CASK"
 is brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # install homebrew cask
-pr "Installing homebrew cask.."
+step "HOMEBREW CASK"
 brew tap caskroom/cask
 
 # Install wget,vim
-pr "Installing some utilities.."
-is vim || brew install vim
-is wget || brew install wget
-is ag || brew install the_silver_searcher
-is tmux || brew install tmux
-is reattach-to-user-namespace || brew install reattach-to-user-namespace
+step "UTILITIES"
+brewy vim
+brewy wget
+brewy ag
+brewy tmux
+brewy reattach-to-user-namespace
 
 # Install nodejs
-pr "Installing nodejs.."
+pr "Installing nodejs"
 is node || brew install node
 
 # Install bashy
-pr "Installing bashy.."
+pr "Installing bashy"
 is bashy || npm install --global bashy
 
 # Install powerline fonts
-pr "Installing powerline fonts.."
+pr "Installing powerline fonts"
 git clone https://github.com/powerline/fonts /tmp/fonts
 /tmp/fonts/install.sh
 
 # Dotfiles
-pr "Configuring dotfiles.."
+step "CONFIG FILES"
 git clone https://github.com/pablopunk/dotfiles ~/.dotfiles
 ~/.dotfiles/install.sh
 
 # Install apps
-pr "Install mac apps.."
+step "MAC APPS"
 while read line; do
-  brew cask install $line;
+  casky $line;
 done < <(curl -sL https://gist.github.com/pablopunk/048e164bb0fd2920711483029d9cc915/raw)
 
 echo -e "$green${bold}✓ DONE!$normal"
