bb5139cbfed911f89c79c1dc13bad7cd4847b96a
pablopunk <pablovarela182@gmail.com>
2018-01-03 23:51:26 +0100

    Make the repo configurable\!

--- mac-fresh-install.sh
+++ mac-fresh-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright Pablo Varela 2016
+# Copyright Pablo Varela 2017
 
 bold="\x01$(tput bold)\x02"
 normal="\x01$(tput sgr0)\x02"
@@ -7,6 +7,9 @@ cyan="\x01\033[36m\x02"
 green="\x01\033[32m\x02"
 step_symbol="❤︎"
 pr_symbol="↪"
+github_raw="https://github.com/pablopunk/mac-fresh-install/blob/master/"
+dotfiles_repo="https://github.com/pablopunk/dotfiles"
+dotfiles_folder="$HOME/.dotfiles"
 
 function pr {
   echo -e "$cyan$bold$pr_symbol $1$normal"
@@ -21,13 +24,35 @@ function is {
 }
 
 function brewy {
-  pr "Installing $1"
+  pr "Installing tool '$1'"
   is $1 || brew install $1
 }
 
 function casky {
-  pr "Installing $1"
+  pr "Installing app '$1'"
   brew cask install $1 2> /dev/null
+}
+
+function npmy {
+  pr "Installing module '$1'"
+  npm i -g $1 > /dev/null
+}
+
+function install_from_github {
+  while read line; do
+    ${1}y $1
+  done < <(curl -sL "$github_raw/$1")
+}
+
+function install_dotfiles {
+  git clone $dotfiles_repo $HOME/.dotfiles
+  bash $HOME/.dotfiles/install.sh
+}
+
+function keyboard_config {
+  defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false && \
+  defaults write NSGlobalDomain KeyRepeat -int 2 && \
+  defaults write NSGlobalDomain InitialKeyRepeat -int 10
 }
 
 # Install command line tools
@@ -41,53 +66,27 @@ is brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Home
 # install homebrew cask
 step "Homebrew cask (app manager)"
 brew tap caskroom/cask
+brew tap caskroom/fonts
 
 # Utils
 step "More command line tools"
-is nvim || brewy neovim
-brewy ctags
-brewy fd
-brewy wget
-brewy ag
-brewy koekeishiya/formulae/kwm
-brewy tmux
-brewy reattach-to-user-namespace
-brewy node
-brewy mosh
-brewy fzy
-npm config set prefix "$HOME/.npm-global"
+install_from_github brew
 
-# Install bashy
-pr "Installing bashy"
-is bashy || npm install --global bashy
+# Npm modules
+step "Npm global modules"
+npm config set prefix "$HOME/.npm-global" && \
+install_from_github npm
 
-# Install fonts
-step "Fonts"
-pr "Powerline fonts"
-git clone https://github.com/powerline/fonts /tmp/fonts
-bash /tmp/fonts/install.sh
-brew tap caskroom/fonts
-pr "Fira code"
-casky font-fira-code
+# Install apps
+step "Apps"
+install_from_github cask
 
 # Dotfiles
 step "Configuration"
 pr "Dotfiles"
-git clone https://github.com/pablopunk/dotfiles $HOME/.dotfiles
-[ -d "$HOME/.dotfiles" ] && bash $HOME/.dotfiles/install.sh
-pr "Keyboard configuration"
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false && \
-defaults write NSGlobalDomain KeyRepeat -int 2 && \
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
-pr "Trackpad configuration"
-sudo defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true && \
-sudo defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1 && \
-sudo defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1 && \
+[ -d "$dotfiles_folder" ] || install_dotfiles
 
-# Install apps
-step "Apps"
-while read line; do
-  casky $line;
-done < <(curl -sL https://gist.github.com/pablopunk/048e164bb0fd2920711483029d9cc915/raw)
+pr "Keyboard configuration"
+keyboard_config
 
 echo -e "$green${bold}✓ DONE! You should restart your computer to get everything working as expected.$normal"
